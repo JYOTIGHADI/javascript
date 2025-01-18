af8f3f19d771290b7ebd7f425892ba0130bf83d1
JYOTIGHADI <jyotighadi2303@gmail.com>
2025-01-18 23:39:09 +0530

    update to day2

--- day 1/index.js
+++ day 1/index.js
@@ -1,1 +1,71 @@
-console.log(Awdiz);
+
+console.log(typeof "Awdiz", "Awdiz");
+console.log(typeof "Awdiz", "Awdiz");
+console.log(typeof `Awdiz`, `Awdiz`);
+
+console.log(typeof 12.21, 12.21);
+
+console.log(typeof true, true);
+
+console.log(12 + 12)
+var myUserName = "Awdiz";
+
+console.log(myUserName, typeof myUserName);
+
+var myAge = 25;
+console.log(myAge, "myAge");
+
+var isOpen = true;
+console.log(isOpen, "isOpen");
+
+var number1 = 100;
+var number2 = 101;
+console.log(number1 + number2);
+
+var number = 20;
+// declare + assign;
+console.log(number);
+number = 21;
+// re - assign;
+console.log(number);
+var number = 22; // re - declare
+console.log(number);
+let myNumber = 30; // declare and assign
+
+console.log(myNumber);
+
+myNumber = 31; // re-assign
+
+console.log(myNumber);
+// re declare
+
+console.log(myNumber);
+
+
+
+
+
+
+
+myNumber = 21;
+
+console.log(myNumber);
+    
+  
+  myAge = 20;
+  
+  console.log(myAge);
+  
+  
+  
+
+  
+  console.log(2 == '2');
+  console.log(2 === 2);
+  
+  console.log(2 != 3); //left side is NOT equal to right side  -> false
+  console.log(2 == 2); // left side is equal to right side -> true
+  
+  console.log(2 != 3);
+  console.log("3" != 3);// left side is NOT equal to right side
+  console.log("3" !== 3);// left side is NOT equal to right side
